b2ae320596eb4447015d54efb2c47cb3596a4832
glenn@home <wuglenn.wg@gmail.com>
2024-08-28 22:22:42 +0800

    attempt to fix useEffect issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,20 +3,21 @@ import pb from '@/lib/pocketbase';
 const getExpireString = (exhours) => {
   const d = new Date();
   d.setTime(d.getTime() + (exhours * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
+  let expires = "expires=" + d.toUTCString();
   return expires
 }
 
-function getCookie(c_name) {
-  var i, x, y, ARRcookies = document.cookie.split(";");
-  for (i = 0; i < ARRcookies.length; i++) {
-      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-      y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-      x = x.replace(/^\s+|\s+$/g, "");
-      if (x == c_name) {
-          return unescape(y);
+function getCookie(cookieName) {
+  if (document) {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+      const [name, value] = cookie.trim().split('=');
+      if (name === cookieName) {
+        return decodeURIComponent(value);
       }
+    }
   }
+  return null;
 }
 
 export async function signUp(email, password, name) {
@@ -50,18 +51,18 @@ export async function signIn(email, password) {
   try {
     const authData = await pb.collection('users').authWithPassword(email, password);
     // console.log('Auth successful, authData:', authData);
-    
-    // Log the auth store state
+
     // console.log('Auth store state after login:', pb.authStore.isValid, pb.authStore.token);
-    
-    // Explicitly set the cookie
-    const cookieString = pb.authStore.exportToCookie({ httpOnly: false });
-    document.cookie = cookieString;
-    let expires = getExpireString(72);
-    document.cookie = 'is_user=1;' + expires + ';path=/;';
-    document.cookie = 'is_merchant=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    if (document) {
+      const cookieString = pb.authStore.exportToCookie({ httpOnly: false });
+      document.cookie = cookieString;
+      let expires = getExpireString(72);
+      document.cookie = 'is_user=1;' + expires + ';path=/;';
+      document.cookie = 'is_merchant=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    }
+
     // console.log('Cookie set:', cookieString);
-    
+
     return authData;
   } catch (error) {
     console.error('Sign in error:', error);
@@ -73,13 +74,15 @@ export async function signInMerchant(email, password) {
   try {
     const authData = await pb.collection('merchants').authWithPassword(email, password);
     // Explicitly set the cookie
-    const cookieString = pb.authStore.exportToCookie({ httpOnly: false });
-    document.cookie = cookieString;
-    
-    let expires = getExpireString(72);
-    document.cookie = 'is_merchant=1;' + expires + ';path=/;';
-    document.cookie = 'is_user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    
+    if (document) {
+      const cookieString = pb.authStore.exportToCookie({ httpOnly: false });
+      document.cookie = cookieString;
+
+      let expires = getExpireString(72);
+      document.cookie = 'is_merchant=1;' + expires + ';path=/;';
+      document.cookie = 'is_user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    }
+
     return authData;
   } catch (error) {
     console.error('Sign in error:', error);
@@ -90,12 +93,14 @@ export async function signInMerchant(email, password) {
 
 export function signOut() {
   pb.authStore.clear();
-  
+
   // Clear the pb_auth cookie
-  document.cookie = 'pb_auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-  document.cookie = 'is_user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-  document.cookie = 'is_merchant=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-  
+  if (document) {
+    document.cookie = 'pb_auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    document.cookie = 'is_user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    document.cookie = 'is_merchant=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+  }
+
   // console.log('Signed out, auth store cleared and cookie removed');
 }
 
